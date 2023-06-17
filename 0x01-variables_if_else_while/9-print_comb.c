9765b79c22bc6e69f7768a10313b1e8dd38af981
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-17 00:35:22 -0700

    me

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	/* your code goes there */
+	while (n < 59)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

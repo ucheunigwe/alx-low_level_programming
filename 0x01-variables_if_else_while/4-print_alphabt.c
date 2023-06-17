c36a88e2df47f5c499888fe65713f07c53829aa5
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-17 00:02:01 -0700

    me

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
+	char n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = 'a';
 	/* your code goes there */
+	while (n <= 'z')
+	{
+		if ((n != 'q') && (n != 'e'))
+			putchar(n);
+		n++;
+	}
+	putchar('\n');
 	return (0);
 }

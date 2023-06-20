591935d22d1418e7c6268f33b5acc64a94f123a0
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-20 11:28:43 -0700

    me

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	/* your code goes there */
+	puts("_putchar")
+	return (0);
+}

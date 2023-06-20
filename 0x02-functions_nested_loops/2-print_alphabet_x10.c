6fd1cb04d4e2b9a6ffcf142dd280a52e3bc61c74
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-20 14:21:31 -0700

    me

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * void print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+
+	/* your code goes there */
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+	}
+}

f795c5f885a6d57b765967a96c6789da4c5f6e9c
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-21 03:40:49 -0700

    me

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _abs - Entry point
+ *
+ * Return: Always int
+ * @n: Param
+ */
+int _abs(int n)
+{
+	return abs(n);
+
+}

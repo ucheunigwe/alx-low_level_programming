785a5941debcb387b0fe754ec8ffcdf873930ede
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-21 01:46:38 -0700

    me

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,15 @@
 
 /* betty style doc for function main goes there */
 /**
- * _islower(int c) - Entry point
+ * _islower - Entry point
  *
  * Return: Always int
+ * @c: Param
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }

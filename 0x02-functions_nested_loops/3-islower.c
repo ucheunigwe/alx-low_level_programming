0cd4ee5292ec1bb736313eaf1819315ed0f2e72f
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-20 18:51:16 -0700

    me

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _islower(int c) - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	return islower(c)
+}

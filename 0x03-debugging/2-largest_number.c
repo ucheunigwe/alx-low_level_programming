a5de6fcca607a274038a753ef15d7a47e7336f63
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-21 18:34:06 -0700

    me

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
+	largest = a;
+
+	if (b > largest)
 	{
 		largest = b;
 	}
-	else
+	else if (c > largest)
 	{
 		largest = c;
 	}

94c3497912b723d910d1cbffb377f67ec24d325d
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-22 23:06:10 -0700

    me

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_square - Entry point
+ * @n : me
+ * Return: Always 0 (Success)
+ */
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	j = 0;
+	while (j < n)
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}

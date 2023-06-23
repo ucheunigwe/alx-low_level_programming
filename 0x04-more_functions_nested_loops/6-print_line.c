bdd883408cd5de8a6a6f89f6adedaddd61c9865e
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-22 22:44:43 -0700

    me

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_line - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}

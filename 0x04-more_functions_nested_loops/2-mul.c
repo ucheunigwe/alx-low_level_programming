341681f8218d150f30843199459fad0d5717fafd
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-22 21:03:13 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * mul - Entry point
+ *
+ * Return: Always int
+ * @a: Param
+ * @b: Param
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

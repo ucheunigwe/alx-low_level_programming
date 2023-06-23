5004d5cd5c96b80d8d467b3772e1ce2faf4dd083
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-23 01:10:18 -0700

    me

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int j;
+
+	j = 1;
+	while (j <= 100)
+	{
+		if ((j % 3 != 0) && (j % 5 != 0))
+			printf("%d", j);
+		else
+		{
+			if (j % 3 == 0)
+				printf("Fizz");
+			if (j % 5 == 0)
+				printf("Buzz");
+		}
+		putchar(' ');
+		j++;
+	}
+	
+	putchar('\n');
+	return (0);
+}

a10e8d34529c3bff6a74a9c26585597a65562f14
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-26 13:23:28 -0700

    me

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * reset_to_98 - Entry point
+ *
+ * @n: Param
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

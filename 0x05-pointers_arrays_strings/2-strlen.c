af644e80013cc6b0688265041086ea2c80bc21ad
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-26 13:52:51 -0700

    me

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _strlen - Entry point
+ *
+ * @s: Param
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}

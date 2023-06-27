0a33f581e22eb307cc8e9576269d511d55c1b40a
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-27 01:42:48 -0700

    me

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print _rev - Entry point
+ *
+ * @s: Param
+ */
+void print_rev(char *s)
+{
+	int i;
+	char *end;
+
+	i = strlen(s);
+	end = (s + i - 1);
+
+	while (end >= s)
+	{
+		_putchar(*end);
+		end--;
+	}
+	_putchar('\n');
+
+}

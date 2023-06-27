c2e83af63760c6fdfcd22d24331d230acb964e9c
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-27 06:58:32 -0700

    me

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * puts_half - Entry point
+ *
+ * @str: Param
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	j = strlen(str);
+	if (j % 2 == 0)
+		i = j / 2;
+	else
+		i = (j - 1) / 2;
+
+	for (i = i; i < j; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}

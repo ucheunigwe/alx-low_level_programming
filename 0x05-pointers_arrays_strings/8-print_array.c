9fcb2cbe86fff64c009cdd67c2d55f60d96ffa68
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-27 21:32:52 -0700

    me

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_array - Entry point
+ *
+ * @a: Param
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d\n",*(a + i));
+		else
+			printf("%d, ",*(a + i));
+	}
+}

4b4d7f3845ab123cdf53fcb378fe6655631cff85
Uchenna Unigwe <uunigwe76@gmail.com>
2023-06-29 12:55:41 -0700

    me

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * _strcmp - Entry point
+ *
+ * @s1: Param
+ * @s2: Param
+ * Return: pointer to string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

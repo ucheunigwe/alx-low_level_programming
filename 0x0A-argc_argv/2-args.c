6c8dd3b0e03953acdb800cb500e1e9b1f3830938
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-07 04:11:36 -0700

    me

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * @argc: Param
+ * @argv: Param
+ *
+ */
+int main(int argc, char* argv[])
+{
+	int  i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return(0);
+}

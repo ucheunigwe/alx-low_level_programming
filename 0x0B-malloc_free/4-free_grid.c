628a11cf6974e1592b9b3872acfcfca551e52500
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-11 19:09:40 -0700

    me

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * dealocate_grid - Entry point
+ *
+ * @j: position of row in grid
+ * @ptr: the entire grid
+ */
+void free_grid(int **ptr, int j)
+{
+	int *cpy;
+
+	if (j == 0)
+	{
+		free(*ptr);
+		free(ptr);
+	}
+	else
+	{
+		cpy = *(ptr + j - 1);
+		free_grid(ptr, j - 1);
+		free(cpy);
+	}
+}

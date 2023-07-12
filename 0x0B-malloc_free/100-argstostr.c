3640fd24b4bbb989646fe251d5cc95815241dc5d
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-11 20:32:26 -0700

    me

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * argstostr- Entry point
+ *
+ * @ac: position of row in grid
+ * @av: the entire grid
+ *
+ * Return: success
+ */
+char *argstostr(int ac, char **av)
+{
+	char *cpy;
+	int len;
+	int i;
+
+	len = 0;
+	if (ac == 0 || av == NULL)
+		cpy = NULL;
+	else
+	{
+		for (i = 0; i < ac; i++)
+		{
+			len += (int) strlen(*(av + i));
+		}
+		cpy = malloc((len * sizeof(char) + ac));
+		*cpy = '\0';
+		for (i = 0; i < ac; i++)
+		{
+			strcat(cpy, *(av + i));
+			len = (int) strlen(cpy);
+			if (len != 0)
+				*(cpy + len) = '\n';
+		}
+
+	}
+	return (cpy);
+}

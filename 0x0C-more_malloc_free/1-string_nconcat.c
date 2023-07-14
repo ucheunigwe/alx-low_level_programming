c7b7b1170f300ed8c65902f90ceb84ccf38c3d69
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-13 20:19:20 -0700

    me

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,23 +4,22 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * malloc_checked - Entry point
+ * string_nconcat - Entry point
  *
- * @b: size of Array to be created
+ * @s1: size of Array to be created
+ * @s2: size of Array to be created
+ * @n: size of Array to be created
  *
  * Return: Always
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0;
-       	unsigned int j = 0;
-       	unsigned int len1 = 0;
-       	unsigned int len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while(s1 && s1[len1])
+	while (s1 && s1[len1])
 		len1++;
-	while(s2 && s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));

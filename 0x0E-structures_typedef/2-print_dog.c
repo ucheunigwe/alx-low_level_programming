3467389a80f2602936c2da0795d918f4cb68e356
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-17 11:05:48 -0700

    me

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#ifndef du
+#define du
+#include "dog.h"
+#include <stdio.h>
+#endif
+
+/**
+ * print_dog - check the code
+ *
+ * @d: Param
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", (d->name));
+		else
+			printf("Name: nil\n");
+		if (d->age)
+			printf("Age: %f\n", (d->age));
+		else
+			printf("Age: nil\n");
+		if (d->owner)
+			printf("Owner: %s\n", (d->owner));
+		else
+			printf("Owner: nil\n");
+	}
+}

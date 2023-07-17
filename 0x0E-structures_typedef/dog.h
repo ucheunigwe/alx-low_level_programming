fa092530b15d27f191bf55d4965771048db61c5d
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-17 07:21:27 -0700

    me

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef foo
 #define foo
+/**
+ * struct my_struct - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog{
 	char *name;
 	float age;

153b8a3bb4babdf5d889ccd3cb6b01f7345b9a49
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-19 12:32:56 -0700

    me

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * print_name - Entry point
+ *
+ * @array: Param
+ * @size: Param
+ * @action: Param
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if ((action != NULL) && (array != NULL) && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
+	}
+}

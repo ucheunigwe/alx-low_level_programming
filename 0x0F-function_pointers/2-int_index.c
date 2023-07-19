59ea5237c9640baf09211dfcef5d09cdb428460c
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-19 13:02:47 -0700

    me

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <string.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * int_index - Entry point
+ *
+ * @array: Param
+ * @size: Param
+ * @cmp Param
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if ((cmp != NULL) && (array != NULL))
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(*(array + i)))
+				return (i);
+		}
+	}
+	return (-1);
+}

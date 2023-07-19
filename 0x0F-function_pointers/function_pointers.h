ec379839ce1f0ade7723721b1a6f7170372de850
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-19 11:53:44 -0700

    me

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef me
+#define me
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -7,3 +9,4 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+#endif

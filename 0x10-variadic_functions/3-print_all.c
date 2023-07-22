9e9240810204ce976f1307758e3ac4a7b08a1f7f
Uchenna Unigwe <uunigwe76@gmail.com>
2023-07-21 23:14:30 -0700

    me

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,46 +15,43 @@ void print_all(const char * const format, ...)
 	char *str, *sep = "";
 	int i = 0;
 
-	if (format == NULL)
+	if (format != NULL)
 	{
-		printf("(nil)\n");
-		return;
-	}
-	va_start(param, format);
-	while (format[i])
-	{
-		switch (format[i])
+		va_start(param, format);
+		while (format[i])
 		{
-			case 'c':
-				{
-					printf("%s%c", sep, va_arg(param, int));
-					break;
-				}
-			case 'i':
-				{
-					printf("%s%d", sep, va_arg(param, int));
-					break;
-				}
-			case 'f':
-				{
-					printf("%s%f", sep, va_arg(param, double));
-					break;
-				}
-			case 's':
-				{
-					str = va_arg(param, char*);
-					if (str == NULL)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				}
-			default:
-				i++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					{
+						printf("%s%c", sep, va_arg(param, int));
+						break;
+					}
+				case 'i':
+					{
+						printf("%s%d", sep, va_arg(param, int));
+						break;
+					}
+				case 'f':
+					{
+						printf("%s%f", sep, va_arg(param, double));
+						break;
+					}
+				case 's':
+					{
+						str = va_arg(param, char*);
+						if (str == NULL)
+							str = "(nil)";
+						printf("%s%s", sep, str);
+						break;
+					}
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+			va_end(param);
 		}
-		sep = ", ";
-		i++;
-		va_end(param);
-	}
-	printf("\n");
+	}	printf("\n");
 }

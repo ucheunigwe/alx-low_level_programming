7fa10857a6da5f146ac74d8bf56a1a7cf66dc4ba
Unigwe Uchenna <uunigwe76@gmail.com>
2023-07-27 05:18:18 -0700

    me

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - prints all the strings in a linked list
+ * list_len - prints the number of nodes in a linked list
  * @h: pointer to the linked list head
  * Return: Number of nodes in the list
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
@@ -16,11 +16,6 @@ size_t print_list(const list_t *h)
 		list_t *temp = (list_t *)h;
 
 		do {
-			if (temp->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%d] %s\n", temp->len, temp->str);
-			temp = temp->next;
 			n++;
 		} while (temp);
 	}

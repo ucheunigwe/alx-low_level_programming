e135477f339ea450224060ed42ab13ac017f6526
Unigwe Uchenna <uunigwe76@gmail.com>
2023-07-29 10:45:18 -0700

    me

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,11 @@
-msg db 'Hello, Holberton',10    ; The string to be printed
-
-global main
-extern printf
+msg: db `Hello, Holberton\n`,0
+global    main
+extern    printf
 
 main:
-	mov   edi, msg
-	xor   eax, eax
-	call  printf
-	mov 	eax, 0
-	ret
+	  mov   edi, msg
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
 

6978d4eb7cbc00f7ede3a89a54228cbb27446437
Unigwe Uchenna <uunigwe76@gmail.com>
2023-07-29 05:34:12 -0700

    me

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  */
 __attribute__((constructor))void myConstructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
 

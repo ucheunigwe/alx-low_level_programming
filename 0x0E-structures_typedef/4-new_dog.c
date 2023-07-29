2331f14c29712eddc4db46bad24491ba8c509100
Unigwe Uchenna <uunigwe76@gmail.com>
2023-07-29 08:24:54 -0700

    me

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,26 +20,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t *));
 	if (d != NULL)
 	{
-		d->name = malloc(sizeof(char) * (strlen(name) + 1));
+		d->name = strdup(name);
 		if (d->name == NULL)
 		{
 			free(d);
 			return (NULL);
 		}
-		d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+		d->owner = strdup(owner);
 		if (d->owner == NULL)
 		{
 			free(d->name);
 			free(d);
 			return (NULL);
 		}
-
-
-		d->name = strdup(name);
-		d->owner = strdup(owner);
 		d->age = age;
 		return (d);
 	}
 	else
+	{
+		free(d);
 		return (NULL);
+	}
 }

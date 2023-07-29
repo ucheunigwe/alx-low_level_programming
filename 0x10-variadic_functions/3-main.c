4e1ee10a2c26d5b1e13555579f3aa7ad7bea476a
Unigwe Uchenna <uunigwe76@gmail.com>
2023-07-29 07:26:24 -0700

    me

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_all(NULL);
+    print_all("ceisz", 'B', 3, "stSchool");
     return (0);
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,10 +9,48 @@
  *
  * @format: Param
  */
+void change(char c,char *sep, va_list param)
+{
+	switch (c)
+	{
+		case 'c':
+			{
+				printf("%s%c", sep, va_arg(param, int));
+				break;
+			}
+		case 'i':
+		{
+			printf("%s%d", sep, va_arg(param, int));
+			break;
+		}
+		case 'f':
+		{
+			printf("%s%f", sep, va_arg(param, double));
+			break;
+		}
+		case 's':
+		{
+			char *str = va_arg(param, char*);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", sep, str);
+			break;
+		}
+		default:
+			break;
+	}
+}
+
+
+/**
+ * print_all - Entry point
+ *
+ * @format: Param
+ */
 void print_all(const char * const format, ...)
 {
 	va_list param;
-	char *str, *sep = "";
+	char *sep = "";
 	int i = 0;
 
 	if (format != NULL)
@@ -20,38 +58,11 @@ void print_all(const char * const format, ...)
 		va_start(param, format);
 		while (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					{
-						printf("%s%c", sep, va_arg(param, int));
-						break;
-					}
-				case 'i':
-					{
-						printf("%s%d", sep, va_arg(param, int));
-						break;
-					}
-				case 'f':
-					{
-						printf("%s%f", sep, va_arg(param, double));
-						break;
-					}
-				case 's':
-					{
-						str = va_arg(param, char*);
-						if (str == NULL)
-							str = "(nil)";
-						printf("%s%s", sep, str);
-						break;
-					}
-				default:
-					i++;
-					continue;
-			}
+			change(format[i], sep, param);
 			sep = ", ";
 			i++;
 			va_end(param);
 		}
-	}	printf("\n");
+	}	
+	printf("\n");
 }

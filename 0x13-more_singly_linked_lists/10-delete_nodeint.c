c438dbbf97ae685df415f701bcfce1c71bbb0861
Unigwe Uchenna <uunigwe76@gmail.com>
2023-08-01 19:44:39 -0700

    me

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,60 +1,36 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * listint_len - prints the number of nodes in a linked list
- * @h: pointer to the linked list head
- * Return: Number of nodes in the list
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t n = 0;
-
-	if (h == NULL)
-		n = 0;
-	else
-	{
-		listint_t *temp = (listint_t *)h;
-
-		do {
-			temp = temp->next;
-			n++;
-		} while (temp);
-	}
-	return (n);
-}
-/**
- * insert_nodeint_at_index - prints the number of nodes in a linked list
+ * delete_nodeint_at_index - prints the number of nodes in a linked list
  * @head: pointer to the linked list head
  * @index: index to return
- * @data: data to insert
  * Return: pointer to the node at index
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index,
-	int data)
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int n;
+	unsigned int i;
 	listint_t *temp, *temp2;
-
-	n = listint_len(*head);
-	if (n < index)
-		temp = NULL;
-	else
+	if (!*head)
+		return (-1);
+	temp = *head;
+	if (index == 0)
 	{
-		temp = *head;
-		index--;
-		while (index)
-		{
-			temp = temp->next;
-			index--;
-		}
-		temp2 = malloc(sizeof(listint_t));
-		if (temp2)
-		{
-			temp2->n = data;
-			temp2->next = temp->next;
-			temp->next = temp2;
-		}
-
+		temp2 = (*head);
+		*head = (*head)->next;
+		free(temp2);
+		return (1);
 	}
-	return (temp2);
+	for (i = 0; (i < index - 1); i++)
+	{
+		if (!temp || !temp->next)
+		{
+			return (-1);
+		}
+		else
+			temp = temp->next;
+	}
+
+	temp2 = temp->next;
+	temp->next = temp2->next;
+	free(temp2);
+	return (1);
 }

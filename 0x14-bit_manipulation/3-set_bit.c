f3386ead598b37de32ab66e9303541791bbb9a79
Unigwe Uchenna <uunigwe76@gmail.com>
2023-08-03 04:42:11 -0700

    me

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int cmp;
 
 	cmp = 1;
-	if (n == NULL)
+	if ((n == NULL) ||(index > 63))
 		return (-1);
 	cmp = cmp << index;
 	*n = *n | cmp;

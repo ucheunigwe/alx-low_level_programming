4dd0891b99278948e3f537e94b81cf20a10b7f76
Unigwe Uchenna <uunigwe76@gmail.com>
2023-08-26 01:51:32 -0700

    me

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -37,31 +37,30 @@ void read_elf_header(const char *file_path, ElfHeader *header) {
 
 int main(int argc, char *argv[]) 
 {
+	const char *file_path = argv[1];
 	ElfHeader elf_header;
-	const char *file_path;
     
 	if (argc != 2) {
         printf("Usage: %s <elf_file>\n", argv[0]);
         return 1;
     }
 
-    file_path = argv[1];
     read_elf_header(file_path, &elf_header);
 
-    printf("ELF Header Information:\n");
-    printf("Type: %u\n", elf_header.type);
-    printf("Machine: %u\n", elf_header.machine);
-    printf("Version: %u\n", elf_header.version);
-    printf("Entry point: 0x%x\n", elf_header.entry);
-    printf("Program header offset: %u\n", elf_header.phoff);
-    printf("Section header offset: %u\n", elf_header.shoff);
-    printf("Flags: 0x%x\n", elf_header.flags);
-    printf("ELF header size: %u\n", elf_header.ehsize);
-    printf("Program header entry size: %u\n", elf_header.phentsize);
-    printf("Program header entry count: %u\n", elf_header.phnum);
-    printf("Section header entry size: %u\n", elf_header.shentsize);
-    printf("Section header entry count: %u\n", elf_header.shnum);
-    printf("Section header string table index: %u\n", elf_header.shstrndx);
+    printf("ELF Header:\n");
+    printf("  Type:                             0x%x\n", elf_header.type);
+    printf("  Machine:                          0x%x\n", elf_header.machine);
+    printf("  Version:                          0x%x\n", elf_header.version);
+    printf("  Entry point address:              0x%x\n", elf_header.entry);
+    printf("  Start of program headers:         %u (bytes into file)\n", elf_header.phoff);
+    printf("  Start of section headers:         %u (bytes into file)\n", elf_header.shoff);
+    printf("  Flags:                            0x%x\n", elf_header.flags);
+    printf("  Size of this header:              %u (bytes)\n", elf_header.ehsize);
+    printf("  Size of program headers:          %u (bytes)\n", elf_header.phentsize);
+    printf("  Number of program headers:        %u\n", elf_header.phnum);
+    printf("  Size of section headers:          %u (bytes)\n", elf_header.shentsize);
+    printf("  Number of section headers:        %u\n", elf_header.shnum);
+    printf("  Section header string table index: %u\n", elf_header.shstrndx);
 
     return 0;
 }
